772611af6e1c0716a42fd813464598a1e0d117c6
Paul Bonser <pib@paulbonser.com>
2014-01-02 15:56:57 -0600

    Mmove ElementAttributeExtractor to a separate file

--- extract.go
+++ extract.go
@@ -5,31 +5,11 @@ package extract
 
 import (
 	"code.google.com/p/go.net/html"
-	"code.google.com/p/go.net/html/atom"
 	"io"
 )
 
 type Extractor interface {
 	HandleToken(html.Token)
-}
-
-// An ElementAttributeExtractor extracts the specified attribute from
-// each instance of the specified element, and stores their values.
-type ElementAttributeExtractor struct {
-	Element atom.Atom
-	AttrKey string
-	Values  *[]string
-}
-
-func (e ElementAttributeExtractor) HandleToken(token html.Token) {
-	switch token.Type {
-	case html.StartTagToken, html.SelfClosingTagToken:
-		if token.DataAtom == e.Element {
-			if val, exists := Attr(token, e.AttrKey); exists {
-				*e.Values = append(*e.Values, val)
-			}
-		}
-	}
 }
 
 func Extract(r io.Reader, extractor Extractor) error {
@@ -50,13 +30,4 @@ func Extract(r io.Reader, extractor Extractor) error {
 		}
 	}
 
-}
-
-func Attr(token html.Token, key string) (string, bool) {
-	for _, attr := range token.Attr {
-		if attr.Key == key {
-			return attr.Val, true
-		}
-	}
-	return "", false
 }
